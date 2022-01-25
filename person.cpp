7395a9d6fdc59caee525bf508e911381b9ecda74
phamrachel17 <phamrachel17@yahoo.com>
2022-01-24 18:49:10 -0800

    Update person.cpp and personList.cpp files

--- person.cpp
+++ person.cpp
@@ -6,7 +6,7 @@ using std::cout;
 using std::endl;
 
 Person::Person(const char *name_, Person* father_, Person* mother_){
-    name = new char[strlen(name_)];
+    name = new char[strlen(name_) + 1];
     strcpy(name, name_);
     father = father_;
     mother = mother_;
@@ -16,7 +16,8 @@ Person::Person(const char *name_, Person* father_, Person* mother_){
 }
 
 Person::~Person(){
-    delete children;
+    delete [] children;
+    delete [] name;
 }
 
 void Person::addChild(Person *newChild){
@@ -52,6 +53,7 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+    delete [] temp;  //deallocate temp
 }
 
 /* helper function to compute the lineage
@@ -62,10 +64,11 @@ char* Person::compute_relation(int level){
     if(level == 0) return strcpy(new char[1], "");
 
     char *temp = strcpy(new char[strlen("grand ") + 1], "grand ");;
-    
+
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
+        delete [] temp;  //deallocate the temp before it gets reassigned
         temp = temp2;
     }
     return temp;
@@ -78,5 +81,6 @@ void expand(Person ***t, int *MAX){
   Person **temp = new Person*[2 * *MAX];
   memcpy(temp, *t, *MAX * sizeof(**t));
   *MAX *= 2;
+  delete [] *t; // delete *t before it get reassigned
   *t = temp;
 }
